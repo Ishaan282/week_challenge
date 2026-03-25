7d9750b0f7b5aa56a29d3ca0a2c35ad8ea95e231
Ishaan282 <ishaansingla282@gmail.com>
2026-03-25 10:35:25 +0530

    Vertical Text

--- hi.cpp
+++ hi.cpp
@@ -1,15 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void s(int n){
-    while(n > 0){
-        if(n % 10 == 7) cout << "boom ";
-        n = n/10;
-    }
-}
-
 int main(){
-    vector<int> v = {2, 75, 60, 97, 86};
-    for(int it : v)
-        s(it);
+    //inilize the inputs and array 
+    string a , b;
+    cin >> a >> b;
+    int aa = a.size(), bb = b.size(); 
+    int big = (aa >= bb) ? aa : bb;
+    vector<pair<char,char>> vec(big);
+
+    //fill in the array 
+    int i = 0 , j = 0, k = 0;
+    while(i < aa && j < bb){
+        vec[k] = {a[i],b[j]};
+        i++; j++; k++;
+    }
+
+    //fill in remaining elements 
+    if(i < aa - 1){
+        while(i < aa){
+            vec[k] = {a[i], ' '};
+            k++; i++;
+        }
+    } else if(j < bb - 1){
+        while(j < bb){
+            vec[k] = {' ', b[j]};
+            k++; j++;
+        }
+    }
+
+    //printing the whole thing 
+    cout << "output= [" << endl;
+    for(int z = 0 ; z < big ; z++){
+        cout << "[\"" << vec[z].first << "\", \"" << vec[z].second << "\"]," << endl;
+    }
+    cout << "]" << endl;
+
+    return 0;
+
 }
